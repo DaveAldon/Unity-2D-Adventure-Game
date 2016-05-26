0d83ce5e5d2980434da394bbf95842981bd0370b
David Crawford <crawford@mail.gvsu.edu>
2016-05-26 17:01:41 -0400

    Added background image, and added camera controller that follows player after a short dampening amount of time.

---
+++ Assets/Scripts/SavingPlayerLocation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SavingPlayerLocation : MonoBehaviour {
+
+	public Transform target;
+	private Transform playerLocation;
+	bool _hasBegun = false;
+
+	void Start () {
+		if (_hasBegun) {
+			playerLocation.transform.position = new Vector3(0, 0, 0);
+		}
+	}
+	// Update is called once per frame
+
+	void OnGUI () {
+		GUI.Box (new Rect (10, 10, 100, 90), "Location Manager");
+
+		if(GUI.Button(new Rect(20,40,80,20), "Save Location")) {
+			playerLocation.position = new Vector3();
+		}
+
+		if(GUI.Button(new Rect(20,70,80,20), "Load Location")) {
+			target.position = playerLocation.transform.position;
+		}
+	}
+}
