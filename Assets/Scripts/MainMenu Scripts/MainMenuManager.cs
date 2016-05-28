6f2038487fb5d08eaae4d5d9a7df10b1e829f045
DavidAldon <crawford@mail.gvsu.edu>
2016-05-28 17:29:37 -0500

    Main Menu is now added, with comprehensive loading of save data through serialization of custom binary files with extension .gd stored in Unity3D default file save location. Save data consists only of a player name, and then loads a normal game scene with player sprite. The save data still needs to capture player location and load it in.

---
+++ Assets/Scripts/MainMenu Scripts/MainMenuManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+
+	public enum Menu {
+		MainMenu,
+		NewGame,
+		Continue
+	}
+
+	public Menu currentMenu;
+
+	void OnGUI () {
+
+		GUILayout.BeginArea(new Rect(0,0,Screen.width, Screen.height));
+		GUILayout.BeginHorizontal();
+		GUILayout.FlexibleSpace();
+		GUILayout.BeginVertical();
+		GUILayout.FlexibleSpace();
+
+		if(currentMenu == Menu.MainMenu) {
+
+			GUILayout.Box("2D Aventure Game");
+			GUILayout.Space(10);
+
+			if(GUILayout.Button("New Game")) {
+				Game.current = new Game();
+				currentMenu = Menu.NewGame;
+			}
+
+			if(GUILayout.Button("Continue")) {
+				SaveLoad.Load();
+				currentMenu = Menu.Continue;
+			}
+
+			if(GUILayout.Button("Quit")) {
+				Application.Quit();
+			}
+		}
+
+		else if (currentMenu == Menu.NewGame) {
+
+			GUILayout.Box("Name Your Traveller");
+			GUILayout.Space(10);
+
+			GUILayout.Label("Name");
+			Game.current.traveller.name = GUILayout.TextField(Game.current.traveller.name, 20);
+
+			if(GUILayout.Button("Save")) {
+				SaveLoad.Save(); //Save the current Game as a new saved Game
+				UnityEngine.SceneManagement.SceneManager.LoadScene("1"); //Load Scene 1
+			}
+
+			GUILayout.Space(10);
+			if(GUILayout.Button("Cancel")) {
+				currentMenu = Menu.MainMenu;
+			}
+
+		}
+
+		else if (currentMenu == Menu.Continue) {
+
+			GUILayout.Box("Select Save File");
+			GUILayout.Space(10);
+
+			foreach(Game g in SaveLoad.savedGames) {
+				if(GUILayout.Button(g.traveller.name + " - ")) {
+					Game.current = g;
+					UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+				}
+
+			}
+
+			GUILayout.Space(10);
+			if(GUILayout.Button("Cancel")) {
+				currentMenu = Menu.MainMenu;
+			}
+
+		}
+
+		GUILayout.FlexibleSpace();
+		GUILayout.EndVertical();
+		GUILayout.FlexibleSpace();
+		GUILayout.EndHorizontal();
+		GUILayout.EndArea();
+
+	}
+}
--- Assets/Scripts/SaveLoad.cs
+++ Assets/Scripts/SaveLoad.cs
@@ -8,12 +8,22 @@ public static class SaveLoad {
 	public static List<Game> savedGames = new List<Game>();
 
 	public static void Save() {
-		savedGames.Add(Game.current);
+		savedGames.Add(Game.current); //adds current game session to the list of "savedGames"
 		BinaryFormatter bf = new BinaryFormatter();
-		FileStream file = File.Create (Application.persistentDataPath + "/savedGames.gd");
+		FileStream file = File.Create (Application.persistentDataPath + "/savedGames.gd"); //creates custom file with our own extension "gd"
 		bf.Serialize(file, SaveLoad.savedGames);
 		file.Close();
-	}
-}
 
-//http://gamedevelopment.tutsplus.com/tutorials/how-to-save-and-load-your-players-progress-in-unity--cms-20934
+		//note that Application.persistentDataPath is the default path location of save files for Unity3d. Calling on this allows this code to be multiplatform without worrying about special paths
+	}
+
+	public static void Load() {
+		if(File.Exists(Application.persistentDataPath + "/savedGames.gd")) {
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open (Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+			SaveLoad.savedGames = (List<Game>)bf.Deserialize(file);
+			file.Close();
+		}
+	}
+
+}
