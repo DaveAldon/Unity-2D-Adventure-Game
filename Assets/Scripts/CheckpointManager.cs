ba6473e7732f095b91fbe37217fc4079f0c24732
David Crawford <crawford@mail.gvsu.edu>
2016-06-12 21:10:01 -0400

    Begun work on checkpoint system where the player will walk in vicinity of a checkpoint and be able to save their position to whichever game save is active.

---
+++ Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		if(c.gameObject.tag == "Player") {
+			Destroy(transform.gameObject);
+			Debug.Log("something");
+		}
+	}
+}
