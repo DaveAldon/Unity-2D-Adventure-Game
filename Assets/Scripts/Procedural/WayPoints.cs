6b1523e3600283695e4551b967a9013c144a12ca
David Crawford <crawford@mail.gvsu.edu>
2016-08-14 21:40:53 -0400

    The eye of the storm now follows waypoints around the scene, and the weather followers randomly generate around its coordinates and follow it.

---
+++ Assets/Scripts/Procedural/WayPoints.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WayPoints : MonoBehaviour {
+
+	// put the points from unity interface
+	public Transform[] wayPointList = new Transform[4];
+
+	public int currentWayPoint = 0; 
+	Transform targetWayPoint;
+
+	public float speed = 4f;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// check if we have somewere to walk
+		if(currentWayPoint < this.wayPointList.Length)
+		{
+			if(targetWayPoint == null)
+				targetWayPoint = wayPointList[currentWayPoint];
+			walk();
+		}
+	}
+
+	void walk(){
+		// rotate towards the target
+		transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed*Time.deltaTime, 0.0f);
+
+		// move towards the target
+		transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position,   speed*Time.deltaTime);
+
+		if(transform.position == targetWayPoint.position)
+		{
+			currentWayPoint ++ ;
+			if (currentWayPoint >= wayPointList.Length) {
+				currentWayPoint = 0;
+			}
+			targetWayPoint = wayPointList[currentWayPoint];
+		}
+	} 
+}
--- Assets/Scripts/Procedural/WeatherSpawner.cs
+++ Assets/Scripts/Procedural/WeatherSpawner.cs
@@ -6,27 +6,36 @@ public class WeatherSpawner : MonoBehaviour {
 	public float high = 10.0f;
 	public float low2 = -10.0f;
 	public float high2 = 10.0f;
+
+	public float lowFollowx;
+	public float highFollowx;
+	public float lowFollowy;
+	public float highFollowy;
 			
 	public GameObject leader;
 	public GameObject follower;
-	//public Vector3[] pubPositions = new Vector3[10];
 
 	void Start() {
+		lowFollowx = GameObject.Find ("WeatherLeader").transform.position.x - 2;
+		highFollowx = GameObject.Find ("WeatherLeader").transform.position.x + 2;
+		lowFollowy = GameObject.Find ("WeatherLeader").transform.position.y - 2;
+		highFollowy = GameObject.Find ("WeatherLeader").transform.position.y + 2;
 		generateFollowers();
 	}
-
+	/*
 	public void generateLeader() {
-		Random.InitState (1);
+		//Random.InitState (1);
 		Vector3 eyeOfStormPos = new Vector3(Random.Range (low, high), Random.Range (low2, high2), 0);
 		Instantiate(leader, eyeOfStormPos, Quaternion.identity);
 		}
+		*/
 
 	public void generateFollowers() {
 
 		Vector3[] positions = new Vector3[10]; //We need to declare the array in a method and not the class, or the index won't be able to be accessed correctly
 
 		for (int i = 0; i < 9; i++) {
-			positions[i] = new Vector3(Random.Range (low, high), Random.Range (low2, high2), 0);
+			positions[i] = new Vector3(Random.Range (lowFollowx, highFollowx), Random.Range (lowFollowy, highFollowy), 0);
 		}
 
 		for (int i = 0; i < 10; i++) {
